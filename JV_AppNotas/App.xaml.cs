cba8b848ae4141f319ee47a0907fd159618678f1
javier.varela <javier.varela@udla.edu.ec>
2023-12-06 07:16:54 -0500

    Add project files.

---
+++ JV_AppNotas/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JV_AppNotas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
